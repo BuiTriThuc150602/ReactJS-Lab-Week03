65261c7a718b8d0aeaec723b1ec1f7c4e0c272ff
BuiTriThuc150602 <20088361.thuc@student.iuh.edu.vn>
2023-10-01 02:37:27 +0700

    Uodate New File

--- App.js
+++ App.js
@@ -2,5 +2,11 @@ import FirstScreen from "./src/Displays/FirstScreen";
 import Screen1a from "./src/Displays/Screen1a";
 
 export default function App() {
-  return <Screen1a/>;
+  return(
+    
+    //for test show first screen
+    //<FirstScreen/>
+    //for test show Screcn1a
+    <Screen1a/>
+    );
 }
